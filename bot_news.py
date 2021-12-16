3204a1b918646a495a1270b6fe7d278c2364e0c8
Andrew Laptev <nickname.avast@gmail.com>
2021-12-16 20:03:35 +0300

    Added: logging, button_handler() and add .env variables

--- bot_news.py
+++ bot_news.py
@@ -1,19 +1,38 @@
-from telegram.base import TO
-from telegram.ext import Updater, CommandHandler
-from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
+import os
+import logging
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Updater, CallbackQueryHandler, CallbackContext
 
-USAGE = '/greet <name> - Greet me!'
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+def button_handler(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+    if query.data == "added":
+        f = open("test_ms.txt", "a")
+        f.write(query.message.to_json() + '\n')
+        f.close
+    query.edit_message_text(text=f"Current news: {query.data}")
+
+def main() -> None:
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(BOT_TOKEN)
+    updater.dispatcher.add_handler(CallbackQueryHandler(button_handler))
+    # Start the Bot
+    updater.start_polling()
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
+    updater.idle()
 
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(USAGE)
-
-def greet_command(update: Update, context: CallbackContext):
-    update.message.reply_text(f'Hello {context.args[0]}!')
-
-def send_news(update: Update, context: CallbackContext, text: str):
-    update.message.reply_text(redact_text(text), parse_mode='Markdown')
-
-def redact_text(text: str):
-    return text.replace("**", "***")
+if __name__ == '__main__':
+    main()
