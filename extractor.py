3e6dd6fef6a79fcc5afdeba926ffd4cff7336d29
Andrew Laptev <nickname.avast@gmail.com>
2021-12-17 20:57:09 +0300

    Added parsing list of news channel from channel_list.txt file and generate handlers in for loop

--- extractor.py
+++ extractor.py
@@ -15,11 +15,12 @@ API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BOT_CHAT_ID = os.getenv("BOT_CHAT_ID")
-client = TelegramClient('session_name1', API_ID, API_HASH)
-channel_dict = {
-    'test':'https://t.me/testglobalavatar',
-    'test1':'https://t.me/tegetge',
-}
+client = TelegramClient('session_name', API_ID, API_HASH)
+channel_list = []
+
+with open("channel_list.txt") as f:
+    for line in f:
+        channel_list.append(line)
 
 def telegram_bot_send_news(bot_token: str, bot_chatID: str, news_text: str, button1_name: str, button1_callback_data: str, button2_name: str, button2_callback_data: str):
     headers = {'Content-Type': 'application/json; charset=utf-8',}
@@ -28,18 +29,14 @@ def telegram_bot_send_news(bot_token: str, bot_chatID: str, news_text: str, butt
     data3 = f'"text":"{button2_name}", "callback_data": "{button2_callback_data}"' + '}]]} }'
     data = data1 + data2 + data3
     response = requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage', headers=headers, data=data.replace("**", "***").encode('utf-8'))
-    return json.dumps(response.json(), ensure_ascii=False)
+    return json.dumps(response.json(), sort_keys=True, indent=4, ensure_ascii=False)
 
-@client.on(events.NewMessage(chats = channel_dict.get('test')))
-async def news_event_handler(event):
-    channel_name = '[' + str(event.chat.title).upper() + ']' + f"({channel_dict.get('test')})\n\n"
-    print(telegram_bot_send_news(BOT_TOKEN, BOT_CHAT_ID, channel_name + event.text, "Add", "added", "Delete", "deleted"))
 
-@client.on(events.NewMessage(chats = channel_dict.get('test1')))
-async def news_event_handler(event):
-    channel_name = '[' + str(event.chat.title).upper() + ']' + f"({channel_dict.get('test1')})\n\n"
-    print(telegram_bot_send_news(BOT_TOKEN, BOT_CHAT_ID, channel_name + event.text, "Add", "added", "Delete", "deleted"))
-
+for link in channel_list:
+    @client.on(events.NewMessage(chats = link))
+    async def news_event_handler(event):
+        channel_name = '[' + str(event.chat.title) + ']' + f"({link}/{event.id})\n\n"
+        print(telegram_bot_send_news(BOT_TOKEN, BOT_CHAT_ID, channel_name + event.text, "Add", "added", "Delete", "deleted"))
 
 if __name__ == "__main__":
     client.start()
