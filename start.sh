2eec092a3568d88640298652304192505817a29c
Andrew Laptev <nickname.avast@gmail.com>
2021-12-19 21:49:43 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -u "/home/eternal/Desktop/telegram_news/bot_news.py" & python -u "/home/eternal/Desktop/telegram_news/extractor.py"
+python -u "bot_news.py" & python -u "extractor.py"
