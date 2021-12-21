5f994c618085d5caa3a50e0b3a30bda917fe3b78
Andrew Laptev <nickname.avast@gmail.com>
2021-12-21 21:47:44 +0300

    Correct bot_news.py and delete editing functionality

--- bot_news.py
+++ bot_news.py
@@ -36,28 +36,31 @@ def convert_json(news_pattern: dict, src_news: dict):
 def button_handler(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     query.answer()
-    news = convert_json(news_pattern, json.loads(query.message.to_json()))
-    if query.data == "markup":
-        # news = convert_json(news_pattern, json.loads(query.message.to_json()))
-        # print(json.dumps(json.loads(query.message.to_json()), sort_keys=True, indent=4, ensure_ascii=False))
-        query.edit_message_text('<b>Title</b>:\n' + news['payload']['title'] + '\n\n<b>Abstract</b>:\n' + news['payload']['abstract'], parse_mode='html')
-        # print(query.message.text_html)
+    news_tmp = convert_json(news_pattern, json.loads(query.message.to_json()))
 
-        keyboard = [[InlineKeyboardButton('Add',callback_data='added'), InlineKeyboardButton('Delete',callback_data='deleted')], 
-                    [InlineKeyboardButton('Edit',callback_data='edit')]]
+    if query.data == "markup": # ломает ссылки в абстракте, надо поправить
+        source = '<strong><a href="' + news_tmp['payload']['pdf_url'] + '">' + news_tmp['payload']['authors'][0] + '</a></strong>\n'
+        title = '<b>Title</b>:\n' + news_tmp['payload']['title']
+        abstract = '\n\n<b>Abstract</b>:\n' + news_tmp['payload']['abstract']
+        query.edit_message_text(source + title + abstract, parse_mode='html')
+        keyboard = [[InlineKeyboardButton('Add', callback_data='added'), InlineKeyboardButton('Delete',callback_data='deleted')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         query.edit_message_reply_markup(reply_markup=reply_markup)
+    
     if query.data == "added":
+        clean_src_dict = json.loads(query.message.to_json())
+        clean_src_dict['text'] = clean_src_dict['text'].replace("Title:\n", "").replace("\nAbstract:\n", "")
+        news = convert_json(news_pattern, clean_src_dict)
+        ######### сюда можно вписать
         f = open("test_news.json", "a")
         f.write(json.dumps(news, sort_keys=True, indent=4, ensure_ascii=False) + '\n')
         f.close
+        ######### любой нужный метод отправки новости
         query.delete_message()
+    
     elif query.data == "deleted":
         query.delete_message()
-    elif query.data == "edit":
-        keyboard = [[InlineKeyboardButton('Title',callback_data='title')], [InlineKeyboardButton('Abstract',callback_data='abstract')]]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        query.edit_message_reply_markup(reply_markup=reply_markup)
+
 
 def main() -> None:
     updater = Updater(BOT_TOKEN)
